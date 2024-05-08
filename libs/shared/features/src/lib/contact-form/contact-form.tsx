8075396502d9dba9097cd44195570cc5f4724eff
Olaf Siebert <olaf@biffnewmedia.com>
2024-05-08 14:59:01 +0200

    feat:  Footer legal contact form fav app icons

--- libs/shared/features/src/lib/contact-form/contact-form.tsx
+++ libs/shared/features/src/lib/contact-form/contact-form.tsx
@@ -39,7 +39,7 @@ export function ContactForm() {
       const response = await fetch('/__forms.html', {
         method: 'POST',
         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-        body: encode({'form-name': 'contact-form', ...data}),
+        body: encode({'form-name': 'contact', ...data}),
       })
 
 

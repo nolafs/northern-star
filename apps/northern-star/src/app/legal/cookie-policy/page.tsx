f847cebf2384785b3dc086230d6d4fe705a2558b
Olaf Siebert <olaf@biffnewmedia.com>
2024-10-29 12:26:05 +0100

    feat:  Footer legal contact form fav app icons

--- apps/northern-star/src/app/legal/cookie-policy/page.tsx
+++ apps/northern-star/src/app/legal/cookie-policy/page.tsx
@@ -7,7 +7,7 @@ export default function Page(props: PageProps) {
   return (
     <div className={'mx-auto max-w-4xl text-primary prose lg:prose-xl  text-primary'}>
       <h1>Cookie Policy</h1>
-      <p>At Dog on mars, we value your privacy and strive to be transparent about the data we collect and
+      <p>At Dog On Mars, we value your privacy and strive to be transparent about the data we collect and
         how it is used. This Cookie Policy outlines how we use cookies on our website.</p>
 
       <h2>What are cookies?</h2>
@@ -36,7 +36,7 @@ export default function Page(props: PageProps) {
 
       <h2>Contact Us</h2>
       <p>If you have any questions about our Cookie Policy, please contact us at <a
-        href="mailto:hello@dogonmars.com">hello@northernstar.games</a>.</p>
+        href="mailto:hello@dogonmars.com">hello@dogonmars</a>.</p>
 
       <p>This Cookie Policy was last updated on 08/05/2024.</p>
     </div>
